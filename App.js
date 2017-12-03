df0ec9ad0905684cacbc34d6449851d0c9e3879e
Elisabeth Seite <elisabeth.seite@gmail.com>
2017-12-03 15:27:23 -0500

    Connected to camera using Expo's built in modules

--- App.js
+++ App.js
@@ -1,17 +1,47 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { StackNavigator } from 'react-navigation';
+import CameraApp from './components/CameraApp'
 
-export default class App extends React.Component {
+class HomePage extends React.Component {
+
+  constructor(props){
+    super(props)
+    this.state = {
+
+    }
+    this.openCamera = this.openCamera.bind(this)
+  }
+
+  openCamera(navigate) {
+    console.log('button was pressed')
+    navigate('CameraApp')
+  }
+
   render() {
+
+    const { navigate } = this.props.navigation;
+
     return (
       <View style={styles.container}>
         <Text>Open up App.js to start working on your app!</Text>
         <Text>Changes you make will automatically reload.</Text>
         <Text>Shake your phone to open the developer menu.</Text>
+        <Button
+          onPress={() => this.openCamera(navigate)}
+          title="Snap A Picture"
+          color="#841584"
+          accessibilityLabel="Take a picture a learn"
+        />
       </View>
     );
   }
 }
+
+export default App = StackNavigator({
+  Home: { screen: HomePage },
+  CameraApp: {screen: CameraApp}
+});
 
 const styles = StyleSheet.create({
   container: {
---
+++ README.md
@@ -0,0 +1,3 @@
+### Scientia
+
+A Mobile app that help you understand your world.
